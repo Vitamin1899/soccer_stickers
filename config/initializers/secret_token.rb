743cec72a866cdb867004d48173da7c15385829a
Vitali Marinich <vitalimarinich@gmail.com>
2015-09-15 20:35:06 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SoccerStickers::Application.config.secret_key_base = '03b52b80bffe085457e61a8f525d411e224fbef584ba85fa34e2530d9e8847e639e97683bf0b01674f7e37274ba1d616878040eb54ae5b32855bb553b628d190'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SoccerStickers::Application.config.session_store :cookie_store, key: '_soccer_stickers_session'
